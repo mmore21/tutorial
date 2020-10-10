d62be905451e5c3b6d344badf2ecdba0c19ac281
Mason Moreland <mazc@protonmail.ch>
2020-10-10 07:39:38 -0700

    Remove Project Euler files

--- project_euler/pe_001.py
+++
@@ -1,5 +0,0 @@
-"""
-Project Euler #1 - Multiples of 3 and 5
-Methodology: Sum list comprehension with conditional statements.
-"""
-print(sum(x for x in range(0, 1000) if x % 3 == 0 or x % 5 == 0))
