c6e65b09a1bb31c531f84a1db21eeb48b44f4f07
Mason Moreland <mazc@protonmail.ch>
2020-12-23 19:54:15 -0800

    Organize repository and add MIT license

--- lang/shell/raspberry_pi_flash/pi.py
+++ lang/shell/raspberry_pi_flash/pi.py
@@ -7,7 +7,7 @@ length = ""
 
 
 while(True):
-    subprocess.call("./raspi-motd.sh", shell=True)
+    subprocess.call("./raspberry_pi.sh", shell=True)
     time.sleep(0.5)
     os.system('clear')
     time.sleep(0.5)
